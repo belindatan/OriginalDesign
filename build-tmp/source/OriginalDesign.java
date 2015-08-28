389d57d258290470958977d66eed0874e08b840d
belindatan <btgreentea@gmail.com>
2015-08-28 10:20:01 -0700

    updated program

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,15 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 int y=50;
 int x = 250;
 int z=250;
 int a=300;
 int b=250;
-void setup()
+public void setup()
 {
 size (500,500);
-frameRate (15);
+//frameRate (15);
 }
 
-void draw()
+public void draw()
 {	
 
 
@@ -94,4 +110,13 @@ noStroke();
 rect(230,50,120,40);
 rect(280,150,120,40);
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
